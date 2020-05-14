24e768eea690f8b9881b6f493c4a5198d1609086
github username <github email address>
2020-05-14 11:35:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+const submitData = (name,email) => {
+	const data = {
+		name: name,
+		email, email
+	};
+	const reqObj = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"Accept": "application/json"
+		},
+		body: JSON.stringify(data)
+	};
+	const output = document.getElementById('res');
+
+	return fetch("http://localhost:3000/users", reqObj)
+	.then( res => res.json() )
+	.then( json => output.innerHTML = json.id )
+	.catch( err => output.innerHTML = 'Unauthorized Access');
+}
