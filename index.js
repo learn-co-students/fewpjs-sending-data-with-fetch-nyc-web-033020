6bd43a5f62d6ae11d7993408af372423b17ad99d
theluispestana <theluispestana@gmail.com>
2020-05-14 22:59:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+let formData = {
+  name: "Steve",
+  email: "steve@steve.com"
+}
+
+let configObj = {
+  method: "Post",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+}
+
+let url = "http://localhost:3000/users"
+
+function submitData() {
+  return fetch(url, configObj)
+    .then(res => res.json())
+    .then(json => document.body.textContent = json.id)
+    .catch(err => {
+      alert("cannot compute")
+      document.body.textContent = err.message
+    })
+}
