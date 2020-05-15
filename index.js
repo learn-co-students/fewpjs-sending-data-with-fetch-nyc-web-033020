f4da8340e374c690a6233c4eb2350a9de50c8f84
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-05-15 11:50:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+const url = 'http://localhost:3000'
+
+function submitData(name, email) {
+  const config = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    })
+  }
+  console.log('fetching data')
+  return fetch(url + '/users', config)
+    .then(r => r.json())
+    .then(json => {
+      document.querySelector('body').innerHTML = JSON.stringify(json)
+    })
+    .catch(error => {
+      document.querySelector('body').textContent = JSON.stringify(error.message);
+    })
+}
+
+
+
+submitData('elliott', 'exampleemail')
