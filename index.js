6e43234da25d9da5ff27c5f17a3e023081bb8109
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-05-15 19:58:05 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+
+const submitData = (string1, string2) => {
+  return fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: string1,
+      email: string2
+    })
+  })
+  .then(resp => resp.json())
+  .then(json => {
+    let body = document.querySelector('body')
+    body.innerHTML = json.id
+  })
+  .catch( error => {
+    document.body.innerHTML = error.message
+  })
+};
